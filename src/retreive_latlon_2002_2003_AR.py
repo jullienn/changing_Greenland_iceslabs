fe817620ff0687f60f1037912cf133e1c693c146
Nicolas Jullien <nicolas.jullien@unifr.ch>
2020-11-19 09:47:16 +0100

    Matching between master and individual file is now working! What's left to be done: 1.filter the merge dataset to keep only usefull things 2.routine to save (pickle) 3.run

--- src/retreive_latlon_2002_2003_AR.py
+++ src/retreive_latlon_2002_2003_AR.py
@@ -154,14 +154,27 @@ for indiv_file in onlyfiles:
 
     if (duplicateRowsDF.iloc[1]):
         #if (duplicateRowsDF.iloc[1] is True, the first value is double
-        FV = 'double'
         index_begin=df_master_file[df_master_file['seconds_gps']==value_begin]['index_gps'].iloc[0]
-        #index_end=df_master_file[df_master_file['seconds_gps']==value_end]['index_gps'].iloc[-2]
     elif (~(duplicateRowsDF.iloc[1])):
         # if duplicateRowsDF.iloc[1] is False, the first value is single
-        FV = 'single'
         index_begin=df_master_file[df_master_file['seconds_gps']==value_begin]['index_gps'].iloc[1]
-        #index_end=df_master_file[df_master_file['seconds_gps']==value_end]['index_gps'].iloc[-1]
+        
+    if (duplicateRowsDF.iloc[-1]):
+        #if (duplicateRowsDF.iloc[-1] is True, the last value is double
+        index_end=df_master_file[df_master_file['seconds_gps']==value_end]['index_gps'].iloc[-1]+1
+    elif (~(duplicateRowsDF.iloc[-1])):
+        # if duplicateRowsDF.iloc[-1] is False, the last value is single
+        index_end=df_master_file[df_master_file['seconds_gps']==value_end]['index_gps'].iloc[-2]+1
+    
+    #Select the corresponding slice of df_master_file:
+    df_slice=df_master_file.iloc[index_begin:index_end]
+    
+    #Check if we have duplicates of missing values in this slice df compared to the file being read
+    merged_check=[]
+    merged_check = pd.merge(left=df_file_being_read, right=df_slice, left_on='timearr_floor', right_on='seconds_gps')
+    
+    check_join_duplicates=[]
+    check_join_duplicates=merged_check.drop_duplicates(subset=['time_gps'],keep='first')
     
     pdb.set_trace()
     
@@ -182,60 +195,111 @@ for indiv_file in onlyfiles:
         
     loc_df=index_begin
     i_timearr=0
-    
+    i_ite=0
+
+    #1.Create the empty iterative dataset
+    iterative_dataset=pd.DataFrame({'seconds':pd.Series(np.zeros(1010)),
+                                    'useconds':pd.Series(np.zeros(1010)),
+                                    'lat_gps':pd.Series(np.zeros(1010)),
+                                    'lon_gps':pd.Series(np.zeros(1010)),
+                                    'time_gps':pd.Series(np.zeros(1010)),
+                                    'seconds_gps':pd.Series(np.zeros(1010)),
+                                    'index_gps':pd.Series(np.zeros(1010)),
+                                    'timearr_dec':pd.Series(np.zeros(1010)),
+                                    'jump':pd.Series(np.zeros(1010))})
     pdb.set_trace()
     
-    for i in range(0,len(join_duplicates),1):
-        
-        if (i_timearr>=len(df_file_being_read)):
-            print('break out')
-            break
+    #for i in range(0,len(join_duplicates),1):
+    while (i_timearr<len(df_file_being_read)):
+
+        #if (i_timearr>=len(df_file_being_read)):
+        #    print('break out')
+        #    break
         
         #len(df_file_being_read)
         if ((df_master_file['seconds_gps'].iloc[loc_df])==(df_file_being_read['timearr_floor'].iloc[i_timearr])):
             df_master_file['timearr_dec'].iloc[loc_df]=df_file_being_read['timearr'].iloc[i_timearr]
+            
+            #Create the iterative dataset
+            iterative_dataset.iloc[i_ite]=df_master_file.iloc[loc_df]
+            iterative_dataset['timearr_dec'].iloc[i_ite]=df_file_being_read['timearr'].iloc[i_timearr]
+            iterative_dataset['jump'].iloc[i_ite]=0 #If jump, then = 1
+
+            i_ite=i_ite+1
             i_timearr=i_timearr+1
             loc_df=loc_df+1
             
         elif ((df_master_file['seconds_gps'].iloc[loc_df])!=(df_file_being_read['timearr_floor'].iloc[i_timearr])):
-            #We have jumped one time arr, so not update of i_timearr!
+            pdb.set_trace()
+            #Possibility 1: the jump is in df_master_file
+            if ((df_master_file['seconds_gps'].iloc[loc_df])>(df_file_being_read['timearr_floor'].iloc[i_timearr])):
+                #Create a empty lat/lon point which will be matched
+                #1.Create the iterative dataset
+                iterative_dataset['seconds'].iloc[i_ite]=0
+                iterative_dataset['useconds'].iloc[i_ite]=0
+                iterative_dataset['lat_gps'].iloc[i_ite]=0
+                iterative_dataset['lon_gps'].iloc[i_ite]=0
+                iterative_dataset['time_gps'].iloc[i_ite]=0
+                iterative_dataset['seconds_gps'].iloc[i_ite]=0
+                iterative_dataset['index_gps'].iloc[i_ite]=0
+                iterative_dataset['timearr_dec'].iloc[i_ite]=df_file_being_read['timearr'].iloc[i_timearr]
+                iterative_dataset['jump'].iloc[i_ite]=0 #If jump, then = 1
+   
+                i_ite=i_ite+1
+                i_timearr=i_timearr+1
+                #We do not iterate the index in the df_masterfile
+                
+                #Store the information in df_file_being_read[i_time_arr] on this new line
+                #print('WE HAVE BEEN HERE!')
+                #pdb.set_trace()
+                #We have jumped one time arr, so not update of i_timearr!
+                #i_timearr=i_timearr-1
+                #df_master_file['timearr_dec'].iloc[loc_df]=df_file_being_read['timearr'].iloc[i_timearr]
+                #loc_df=loc_df+1
+                #i_timearr=i_timearr+1
             
-            i_timearr=i_timearr-1
-            #df_master_file['timearr_dec'].iloc[loc_df]=df_file_being_read['timearr'].iloc[i_timearr]
-            loc_df=loc_df+1
-            i_timearr=i_timearr+1
+            #Possibility 2: the jump is in df_file_being_read
+            if ((df_master_file['seconds_gps'].iloc[loc_df])<(df_file_being_read['timearr_floor'].iloc[i_timearr])):
+                #Here must be calculated the mean with the previous index on the previous index
+                # Add a column where I specify this is a jump here!!
+
+                i_timearr=i_timearr-1
+                iterative_dataset.iloc[i_ite]=df_master_file.iloc[loc_df]
+                iterative_dataset['timearr_dec'].iloc[i_ite]=df_file_being_read['timearr'].iloc[i_timearr]
+                iterative_dataset['jump'].iloc[i_ite]=1 #If jump, then = 1
+                
+                i_ite=i_ite+1
+                i_timearr=i_timearr+1
+                loc_df=loc_df+1
+                #We do not iterate the index in the df_masterfile
         
         print("i_timearr: ", i_timearr)
         #print("loc_df: ", loc_df)
-
-
-
-    ##### THERE IS A PROBLEM WITH THE DATE 'may13_03_11.mat' !!! SOLVE THE ISSUE
-    #For the jump in seconds, do compute the mean of lat, lon, time_gps between the 2 to end up with one data point
-    pdb.set_trace()
-    
+                
     #3. Make the correspondance between timearr and seconds and join datasets
     #Set the timearr_floor column to be the index of the dataframe to avoid ambiguity in the pd.merge
     df_file_being_read=df_file_being_read.set_index('timearr_floor')
     
     merged_inner=[]
-    merged_inner = pd.merge(left=df_file_being_read, right=df_master_file, left_on='timearr', right_on='timearr_dec')
+    merged_inner = pd.merge(left=df_file_being_read, right=iterative_dataset, left_on='timearr', right_on='timearr_dec')
     
+    #Now that I have added the jump, I can simple remove the lines having jump=1 and then I am good!
+    #Find the location where I have the jump=0 (i.e. not 1!) and obtain the dataset!
+    df_final=merged_inner.loc[merged_inner['jump']==0]
+    #This is done and it is working!!
+    pdb.set_trace()
+  
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
+    #Add a quality accessment: first and last timearr in df_final must fullfil
+    #the first and last from timearr that are present in the df_file_begin_read.
+    #Also, make sure the length between df_final and df_file_being_read are identical
+    #Also, check that the length of merged_inner=2*1000-(nb of jumps)
+    #Should I check also the number of times I do not have data?
+    #Save the results in a separate text file.
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
     
-    
-    
-    result_join=[]
-    
-    #join along the index
-    result_join=df_file_being_read.join(df_master_file, how='left',lsuffix='_time_arr', rsuffix='_gps_latlontime')
-    
-    # Be careful about the remove duplicates with respect to the time_gps. This
-    # is the only solution I found to indeed remove duplicates but maybe check
-    # on other datasets to be sure that it does the right thing!
-       
-    result_join_without_duplicates=[]
-    result_join_without_duplicates=result_join.drop_duplicates(subset=['index_gps'])
-    
+    #Select only the variables of interest for the data storage
+    df_for_storage=
     #Store everything into one dictionnary (matrix and vectors of data)
     dic_file_being_read=[]
     dic_file_being_read = { "trace_id" : indiv_file.replace(".mat",""),
@@ -244,27 +308,6 @@ for indiv_file in onlyfiles:
     pdb.set_trace()
 
         
-#        ith_seconds_gps=seconds_gps_stored
-#        if (count<1):
-#            #This is the first time we have this seconds_gps
-#            df_master_file['timearr_dec']=df_file_being_read['timearr_floor'].iloc[i]
-#
-#            count=count+1
-#        else:
-#            #This is the second time we have this seconds_gps
-#            
-#            count=0
-#            
-#    
-#    master_df_of_interest['seconds_gps_decimal'].iloc[i]=
-#
-#    #Create the sliced gps dataset
-#    master_df_of_interest=df_master_file.iloc[index_begin:index_end]
-
-        
-        
-        
-
 
 #        #initialize the count
 #        count=0
