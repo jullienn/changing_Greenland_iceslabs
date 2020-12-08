e3b165b2a758517a1763aaae2f124735820feba2
Nicolas Jullien <nicolas.jullien@unifr.ch>
2020-12-08 11:43:13 +0100

    Use of np.flipud() for radar echogram plotting

--- src/Data_visualisation_2002_2003_AR.py
+++ src/Data_visualisation_2002_2003_AR.py
@@ -90,7 +90,7 @@ for folder_year in folder_years:
                     #pdb.set_trace()
                     continue
                 
-                #If figure have already been generated, continue
+                ##If figure have already been generated, continue
                 #filename_to_check='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_data_localisation/'+indiv_file+'.png'
                 #if (os.path.isfile(filename_to_check)):
                 #    print('Figure already existent, move on to the next date')
@@ -123,29 +123,44 @@ for folder_year in folder_years:
                 depths = v * Time / 2.0
                 
                 #2. Select the first 100 meters.
-                depths_100=depths[depths <= 100]
-                radar_echo_100=radar_echo[depths <= 100]
+                #depths_100=depths[depths <= 100]
+                #radar_echo_100=radar_echo[depths <= 100]
                 
                 #Plot the first 100m of radar echogram and lat/lon on map
+                pdb.set_trace()
                 
                 #ticks_yplot=np.around(np.linspace(0, 1400, 432))
                 #ticks_yplot=ticks_yplot.astype(int)
                 #labels_yplot=depths[ticks_yplot]
                 
-                #Should I use flipud or not??? np.flipud()
+                ##############################################################
+                ############### Begin explanations on pcolor #################
+                
+                #Explainations on how pcolor works. I convinced myself doing a small example that I plotted.
+                #Further explanations: https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.pcolor.html#:~:text=pcolor()%20displays%20all%20columns,Similarly%20for%20the%20rows.
+                #Example: I have a matrix Z that I want to plot
+                
+                #Z=[10,3,24,70,                            40,5,48,22
+                #    2,6,87,21,      ----pcolor(Z)--->     2,6,87,21
+                #    40,5,48,22]                           10,3,24,70
+                
+                #I must use np.flipud() to display the data from top to bottom.
+                
+                ################ End explanations on pcolor ##################
+                ##############################################################
                 
                 #Change the size of the figure
                 #pyplot.rcParams["figure.figsize"]=30,30
                 #Plot the data
                 pyplot.figure()
-                color_map=pyplot.pcolor(radar_echo,cmap=pyplot.get_cmap('gray'))#,norm=divnorm)
+                color_map=pyplot.pcolor(np.flipud(radar_echo),cmap=pyplot.get_cmap('gray'))#,norm=divnorm)
                 pyplot.ylabel('Depth [m]')
                 pyplot.xlabel('Horizontal distance')
                 #pyplot.yticks(ticks=ticks_yplot,labels=labels_yplot)
                 #pyplot.xticks(fontsize=20)
                 #pyplot.yticks(fontsize=20)
                 #pyplot.ylim(0, 200)
-                pyplot.title('Radar echogram complete')
+                #pyplot.title('Radar echogram complete: '+indiv_file[0:10])
                 cbar=pyplot.colorbar()
                 cbar.set_label('Signal strength')
                 pyplot.show()
@@ -155,42 +170,43 @@ for folder_year in folder_years:
                 ###############################################################
                 #II. Process and plot radar echogram localisation
                 
-                #Plot dem
+                ##Plot dem
                 #pyplot.figure(figsize=(48,40))
                 #pyplot.imshow(elevDem, extent=grid.extent,cmap='hot_r',norm=divnorm)
                 #pyplot.colorbar(label='Elevation [m]')
                 #pyplot.grid()
-                
-                #Plot radar track
-                #1. reproject the track from WGS 84 to EPSG 3413             
-               
-                #Some index have lat and lon equal to 0 because of jumps in data aggregation.
-                #Replace these 0 by NaNs
+                #
+                ##Plot radar track
+                ##1. reproject the track from WGS 84 to EPSG 3413             
+                #
+                ##Some index have lat and lon equal to 0 because of jumps in data aggregation.
+                ##Replace these 0 by NaNs
                 #lat.replace(0, np.nan, inplace=True)
                 #lon.replace(0, np.nan, inplace=True)
-                
-                #Transform the longitudes. The longitudes are ~46 whereas they should be ~-46! So add a '-' in front of lon
+                #
+                ##Transform the longitudes. The longitudes are ~46 whereas they should be ~-46! So add a '-' in front of lon
                 #lon=-lon
-                
-                #Transform the coordinated from WGS84 to EPSG:3413
-                #Example from: https://pyproj4.github.io/pyproj/stable/examples.html
+                #
+                ##Transform the coordinated from WGS84 to EPSG:3413
+                ##Example from: https://pyproj4.github.io/pyproj/stable/examples.html
                 #transformer = Transformer.from_crs("EPSG:4326", "EPSG:3413", always_xy=True)
                 #points=transformer.transform(np.array(lon),np.array(lat))
-                
+                #
                 #lon_3413=points[0]
                 #lat_3413=points[1]
-                
-                #2. plot the tracks
+                #
+                ##2. plot the tracks
                 #pyplot.scatter(lon_3413, lat_3413)
                 #pyplot.grid()
-                #pyplot.show()
-                
-                #Create the figure name
+                ##pyplot.show()
+                #
+                ##Create the figure name
                 #fig_name=[]
                 #fig_name='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_data_localisation/'+indiv_file+'.png'
-                
-                #Save the figure
+                #
+                ##Save the figure
                 #pyplot.savefig(fig_name)
+                #pyplot.clf()
  
     else:
         print('Folder',folder_year,', continue ...')
