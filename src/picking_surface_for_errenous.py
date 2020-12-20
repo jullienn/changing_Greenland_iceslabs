7d20abd771865771d937c6d796acc16628f5b1a1
JULLIEN Nicolas <nicolas.jullien@unifr.ch>
2020-12-20 18:43:06 +0100

    Creating the function 'picking_surface_for_erreneous.py' to correct for erroneous picking dates.

--- src/picking_surface_for_errenous.py
+++ src/picking_surface_for_errenous.py
@@ -41,8 +41,10 @@ t0 = 0; # Unknown so set to zero
 # self.C / (1.0 + (coefficient*density_kg_m3/1000.0))
 v= 299792458 / (1.0 + (0.734*0.873/1000.0))
 
-plot_radar_echogram_slice='TRUE'
+surf_pick_selection='TRUE'
 
+#N defines the number of different colors I want to use for the elevation plot
+N=10
 ##############################################################################
 ############################## Define variables ##############################
 ##############################################################################
@@ -57,8 +59,13 @@ def _gaussian(x,mu,sigma):
     return np.exp(-np.power((x-mu)/sigma, 2.)/2.)
 
 #This function have been taken from 'IceBridgeGPR_Manager_v2.py
-def kernel_function(traces_input,suggested_pixel):
-    #pdb.set_trace()
+def modified_kernel_function(traces_input,suggested_pixel):
+    
+    #Define the dates that need the surface picking  to be semi-automatic
+    dates_surf_pick_impr=['may24_02_23','may24_02_24','may24_02_25','may30_02_1'
+                          ,'may30_02_2','may30_02_4','may30_02_5','may30_02_6'
+                          ,'may30_02_7','may30_02_13','may30_02_14','may30_02_15'
+                          ,'may30_02_50','may30_02_51']
     
     traces = traces_input
     #Do not take the log10 of traces because 'data have been detrented in the log domain' according to John Paden's email, so I guess they are already log10!
@@ -93,6 +100,7 @@ def kernel_function(traces_input,suggested_pixel):
     # A template graph to use, just have to add in the center vertical index at each point and go from there.
     search_indices_template = np.sum(np.indices((vertical_span_mask.shape[0], 2*MASK_SEARCH_RADIUS)),axis=0) - MASK_SEARCH_RADIUS - MASK_RADIUS
     for i in range(traces.shape[1]):
+        
         # Create an array of indices spanning the top-to-bottom of the MASK_SEARCH_RADIUS, and fanning out MASK_RADIUS above and below that point.
         search_indices = search_indices_template + last_best_index
         # Handle overflow indices if below zero or above max (shouldn't generally happen)... just assign to the top or bottom pixel
@@ -253,31 +261,27 @@ def _get_rid_of_false_surface_jumps(surface_indices):
 ##############################################################################
 
 ##############################################################################
-################### Define function for ice lenses logging ###################
+############### Define function for discrete colorbar display ###############
 ##############################################################################
-#This function if adapted from https://stackoverflow.com/questions/37363755/python-mouse-click-coordinates-as-simply-as-possible
-def onclick(event):
-    #This functions print and save the x and y coordinates in pixels!
-    print(event.xdata, event.ydata)
-    #Fill in the file to log on the information
-    filename_flog='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_radar_slice/flog_icelenses_alldates.txt'
-    f_log = open(filename_flog, "a")
-    f_log.write(str(round(event.xdata,2))+','+str(round(event.ydata,2))+'\n')
-    f_log.close() #Close the quality assessment file when we’re done!
+def discrete_cmap(N, base_cmap=None):
+    """Create an N-bin discrete colormap from the specified input map"""
+    #This piece of code is from: https://gist.github.com/jakevdp/91077b0cae40f8f8244a
+    # Note that if base_cmap is a string or None, you can simply do
+    #    return plt.cm.get_cmap(base_cmap, N)
+    # The following works for string, None, or a colormap instance:
+
+    base = pyplot.cm.get_cmap(base_cmap)
+    color_list = base(np.linspace(0, 1, N))
+    cmap_name = base.name + str(N)
+    return base.from_list(cmap_name, color_list, N)
 ##############################################################################
-################### Define function for ice lenses logging ###################
+############### Define function for discrete colorbar display ###############
 ##############################################################################
 
 #Open, read and close the file of suggested surface picks
 f = open('C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/data/Exclusion_folder/txt/SURFACE_STARTING_PICKS_Suggestions_2002_2003.txt','r')
 lines = [line.strip() for line in f.readlines() if len(line.strip()) > 0]
 f.close()
-
-#Create the file to log on the information
-filename_flog='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_radar_slice/flog_icelenses_alldates.txt'
-f_log = open(filename_flog, "a")
-f_log.write('xcoord'+','+'ycoord'+'\n')
-f_log.close() #Close the quality assessment file when we’re done!
 
 #Define the working environment
 path= 'C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/data'
@@ -289,48 +293,7 @@ folder_years = [ f.name for f in os.scandir(path) if f.is_dir() ]
 for folder_year in folder_years:
     if (folder_year in list(['2002','2003'])):
         print('Treating the year',folder_year)
-
-        ######################################################################
-        #                 Assess the surface pick performance                #
-        ######################################################################
         
-        #Open, read and close the potential ice slabs file
-        f = open('C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_slice_and_loc/potential_iceslabs.txt','r')
-        potential_iceslabs = [line.strip() for line in f.readlines() if len(line.strip()) > 0]
-        f.close()
-        
-        #Read the surface picking quality assessment file
-        header_list=["date_file","quality"]
-        surf_pick_assessment = pd.read_csv('C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/surf_picking_working_boolean_20022003.txt',sep=',',header=None,names=header_list)
-
-        #pdb.set_trace()
-        count_correct_surf_pick=0
-        
-        for potential_iceslabs_file in potential_iceslabs:
-            #Check the potential ice slabs file have a corresponding quality index
-            if(potential_iceslabs_file in list(surf_pick_assessment["date_file"])):
-                
-                #The two following lines are from:
-                #https://stackoverflow.com/questions/42386629/pandas-find-index-of-value-anywhere-in-dataframe
-                line_of_interest=[]
-                line_of_interest=surf_pick_assessment[surf_pick_assessment.isin([potential_iceslabs_file]).any(axis=1)]
-                
-                quality_of_interest=[]
-                quality_of_interest=np.asarray(line_of_interest["quality"])
-                
-                #Check that the potential ice slab file have a quality=1
-                if(quality_of_interest[0]==1):
-                    count_correct_surf_pick=count_correct_surf_pick+1
-                else:
-                    print(potential_iceslabs_file+' is not of good quality')
-        
-        print('\nThe performance of surface picking form ice slabs files is:')
-        print(str(count_correct_surf_pick/len(potential_iceslabs)*100)+' %')
-
-        ######################################################################
-        #                 Assess the surface pick performance                #
-        ######################################################################            
-        pdb.set_trace()
         #Go into the yearly folders 
         folder_year_name=path+'/'+folder_year
         os.chdir(folder_year_name)
@@ -351,7 +314,8 @@ for folder_year in folder_years:
             #pdb.set_trace()
             for indiv_file in onlyfiles:
                 print('Treating file',indiv_file)
-                
+
+                #pdb.set_trace()
                 #If indiv_file is the quality file, continue
                 if (indiv_file[0:7]==('quality')):
                     #pdb.set_trace()
@@ -377,7 +341,7 @@ for folder_year in folder_years:
                     
                     latlontime=data['latlontime']
                     lat=latlontime['lat_gps']
-                    lon=latlontime['lon_gps']                
+                    lon=latlontime['lon_gps']
                 
                 #Select the first 30m of radar echogram
                 #1. Compute the vertical resolution
@@ -388,19 +352,20 @@ for folder_year in folder_years:
                 #self.SAMPLE_DEPTHS = self.radar_speed_m_s * self.SAMPLE_TIMES / 2.0
                 depths = v * Time / 2.0
                 
-                #If plot_radar_echogram_slice is set to 'TRUE', then plot the slice
-                #radar echogram of that date and save it
-                if (plot_radar_echogram_slice=='TRUE'):
+                #If surf_pick_selection is set to 'TRUE', then plot the raw radar
+                # echogram with the surface overlayed AND the radar slice of
+                #that date and save it
+                if (surf_pick_selection=='TRUE'):
                     ##If file have already been created, continue
-                    #filename_to_check='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_radar_slice/'+indiv_file+'.png'
+                    #filename_to_check='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_raw_and_slice/'+indiv_file+'.png'
                     #if (os.path.isfile(filename_to_check)):
                     #    print('Figure already existent, move on to the next date')
                     #    continue
                     
-                    #I. Process and plot radar echogram
+                    #I. Process and radar echogram
                     #I.a. Load the surface suggestion pick (there is no 'Surface'
                     # variable in 2002/2003 dataset such as 2010/2014 datset).
-                     
+
                     # Load the suggested pixel for the specific date
                     #pdb.set_trace()
                     for date_pix in lines:
@@ -415,15 +380,14 @@ for folder_year in folder_years:
                                 #If it has found its suggested pixel, leave the loop
                                 continue
                     
-                    #I.b. Call the kernel_function to pick the surface
-                    surface_indices=kernel_function(radar_echo, suggested_pixel)
+                    #I.b. Call the modified_kernel_function to pick the surface
+                    surface_indices=modified_kernel_function(radar_echo, suggested_pixel)
                     
                     #I.c. Select the radar slice
                     #Define the uppermost and lowermost limits
                     meters_cutoff_above=0
                     meters_cutoff_below=30
                     
-                    #pdb.set_trace()
                     #Get our slice (30 meters as currently set)
                     radar_slice, bottom_indices = _return_radar_slice_given_surface(radar_echo,
                                                                     depths,
@@ -436,75 +400,86 @@ for folder_year in folder_years:
                     # by looking in the variables it the job was done correctly but I have
                     # checked several variables such as idx_above, idx_below, output traces
                     # and it seems okay to me!
-    
-                    ##############################################################
-                    ############### Begin explanations on pcolor #################
+                    pdb.set_trace()
+                    #Create the y vector for plotting
+                    ticks_yplot=np.arange(0,radar_echo.shape[0],200)
                     
-                    #Explainations on how pcolor works. I convinced myself doing a small example that I plotted.
-                    #Further explanations: https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.pcolor.html#:~:text=pcolor()%20displays%20all%20columns,Similarly%20for%20the%20rows.
-                    #Example: I have a matrix Z that I want to plot
+                    #II. Plot radar echogram localisation
+                    #II.a. Create the subplot
+                    #pdb.set_trace()
+                    pyplot.figure(figsize=(48,40))
+                    pyplot.rcParams.update({'font.size': 5})
+                    fig, (ax1, ax2) = pyplot.subplots(2, 1)#, gridspec_kw={'width_ratios': [1, 3]})
+                    fig.suptitle(indiv_file.replace("_aggregated",""))
                     
-                    #Z=[10,3,24,70,                            40,5,48,22
-                    #    2,6,87,21,      ----pcolor(Z)--->     2,6,87,21
-                    #    40,5,48,22]                           10,3,24,70
+                    #II.b. Plot the raw radar slice with the surface highlighted
+                    #II.b.1. Plot the raw radar echogram
+                    cb_1=ax1.pcolor(radar_echo,cmap=pyplot.get_cmap('gray'))#,norm=divnorm)
                     
-                    #I must use np.flipud(), or invert the y axis to display the data from top to bottom.
+                    #II.b.2. Display the picked surface over it
+                    #pdb.set_trace()
+                    ax1.plot(np.arange(0,radar_echo.shape[1]),surface_indices,linestyle='--',color='red',linewidth=0.1)
+                    #II.b.3. Display the bootom picked surface over it
+                    ax1.plot(np.arange(0,radar_echo.shape[1]),np.asarray(bottom_indices).flatten(),linestyle='--',color='magenta',linewidth=0.1)
+                    #II.b.4 Set plot properties
+                    ax1.invert_yaxis() #Invert the y axis = avoid using flipud.
+                    #ax1.set_aspect('equal') # X scale matches Y scale
+                    ax1.set_yticks(ticks_yplot) 
+                    ax1.set_yticklabels(np.round(depths[ticks_yplot]))
+                    ax1.set_title('Raw radar echogram',fontsize=5)
+                    ax1.set_ylabel('Depth [m]')
+                    ax1.set_xlabel('Horizontal distance')                    
+                    cbar1=fig.colorbar(cb_1, ax=[ax1], location='right')
+                    cbar1.set_label('Signal strength', fontsize=5)
+                    #pdb.set_trace()
+                    #II.c. Plot the radar slice (first 30m of radar echogram)
                     
-                    ################ End explanations on pcolor ##################
-                    ##############################################################
-                    
-                    #Log the date we are dealing with in the ice lenses location file
-                    filename_flog='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_radar_slice/flog_icelenses_alldates.txt'
-                    f_log = open(filename_flog, "a")
-                    
-                    if (folder_day=='jun04'):
-                        f_log.write(str(indiv_file.replace(".mat",""))+'\n')
-                    else:
-                        f_log.write(str(indiv_file.replace("_aggregated",""))+'\n')
-
-                    f_log.close() #Close the file
-                    
-                    #Generate the pick for vertical distance display
+                    #Create the y vector for plotting
                     ticks_yplot=np.arange(0,radar_slice.shape[0],20)
                     
-                    #I.d. Plot the radar slice (first 30m of radar echogram)
-                    #pdb.set_trace()
-                    #Plot the data            
+                    #Plot the radar slice
+                    cb=ax2.pcolor(radar_slice,cmap=pyplot.get_cmap('gray'))#,norm=divnorm)
+                    ax2.invert_yaxis() #Invert the y axis = avoid using flipud.
+                    ax2.set_aspect('equal') # X scale matches Y scale
+                    #In order to display the depth, I used the example 1 of the
+                    #following site: https://www.geeksforgeeks.org/matplotlib-axes-axes-set_yticklabels-in-python/
+                    ax2.set_yticks(ticks_yplot) 
+                    ax2.set_yticklabels(np.round(depths[ticks_yplot]))
+                    ax2.set_title('Radar echogram slice',fontsize=5)
+                    ax2.set_ylabel('Depth [m]')
+                    ax2.set_xlabel('Horizontal distance')
+                    cbar=fig.colorbar(cb)
+                    cbar.set_label('Signal strength', fontsize=5)
                     
-                    fig=pyplot.figure(figsize=(40,10))
-                    
-                    #Change label font
-                    pyplot.rcParams.update({'font.size': 40})
-                    
-                    color_map=pyplot.pcolor(radar_slice,cmap=pyplot.get_cmap('gray'))#,norm=divnorm)
-                    pyplot.yticks(ticks=ticks_yplot,labels=(np.round(depths[ticks_yplot])))
-                    pyplot.gca().invert_yaxis() #Imvert the y axis = avoid using flipud.
-                    pyplot.gca().set_aspect('equal') # X scale matches Y scale
-                    pyplot.ylabel('Depth [m]')
-                    pyplot.xlabel('Horizontal distance')
-                    #pyplot.yticks(ticks=ticks_yplot,labels=labels_yplot)
-                    #pyplot.xticks(fontsize=20)
-                    #pyplot.yticks(fontsize=20)
-                    #pyplot.ylim(0, 200)
-                    pyplot.title('Radar echogram slice: '+indiv_file.replace("_aggregated",""))
-
-                    cbar=pyplot.colorbar()
-                    cbar.set_label('Signal strength')
-                    fig.canvas.mpl_connect('button_press_event', onclick)
                     pyplot.show()
                     
-                    #Create the figure name
+                    ##Create the figure name
                     #fig_name=[]
-                    #fig_name='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_radar_slice/'+indiv_file+'.png'
+                    #fig_name='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_raw_and_slice/'+indiv_file+'.png'
                     
                     ##Save the figure
                     #pyplot.savefig(fig_name,dpi=500)
                     #pyplot.clf()
                     
-                    pdb.set_trace()
-                    
-                    continue
-                    
     else:
         print('Folder',folder_year,', continue ...')
         continue
+
+#Open files
+
+
+
+
+#Visualise the data
+
+            #Store everything into one dictionnary (matrix and vectors of data)
+            #if (folder_day == 'jun04'):
+            #    dic_file_being_read=[]
+            #    dic_file_being_read = { "trace_id" : indiv_file.replace(".mat",""),
+            #         "radar_echogram" : file_being_read['data'],
+            #         "latlontime" : df_final}
+            #else:
+            #    dic_file_being_read=[]
+            #    dic_file_being_read = { "trace_id" : indiv_file.replace(".mat",""),
+            #         "radar_echogram" : file_being_read['filtfin'],
+            #         "latlontime" : df_final}
