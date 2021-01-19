c8987320faa7ffbe1ca3f51a6e4d71c18d445122
Nicolas Jullien <nicolas.jullien@unifr.ch>
2021-01-19 12:04:08 +0100

    Display excess melt with contour elevation on top of it. The contour elevations is plotted thanks to Andrew. Need to improve plot because problem with coordinates for excess melt.

--- src/excess_melt_plot.py
+++ src/excess_melt_plot.py
@@ -11,15 +11,12 @@ Read and plot the excess melt calculation made by Andrew Tedstone
 
 #Import packages
 
-import xarray as xr
-import rasterio
-from rasterio.plot import show
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import rasterio as rio
+import geopandas as gpd
+import os
 import numpy as np
-from pysheds.grid import Grid
-
-from osgeo import gdal
-import elevation 
+import xarray as xr
 
 #Define path of the working directory
 global_path='C:/Users/jullienn/Documents/working_environment/'
@@ -28,58 +25,23 @@ global_path='C:/Users/jullienn/Documents/working_environment/'
 wanted_year='2006'
 
 ##########################################################################
-###                   Load Greenland satellite image	and DEM            ###
+###                      Load Greenland DEM and contours               ###
 ##########################################################################
 
-#Load satellite image
-path_image=global_path+'greenland_topo_data/satellite_image/Greenland_natural_90m.tif'
+dem_filename = 'C:/Users/jullienn/Documents/working_environment/greenland_topo_data/elevations/greenland_dem_mosaic_100m_v3.0.tif'
+contours_filename = 'C:/Users/jullienn/Documents/working_environment/greenland_topo_data/elevations/greenland_contours_100m_v3.0.shp'
 
-#im = rasterio.open(path_image)
-#show(im)
-#show(im.read(), transform=im.transform)
-
-#Load the DEM
-grid = Grid.from_raster("C:/Users/jullienn/Documents/working_environment/greenland_topo_data/elevations/greenland_dem_mosaic_100m_v3.0.tif",data_name='dem')
-#Minnor slicing on borders to enhance colorbars
-elevDem=grid.dem[:-1,:-1]              
-
+with rio.open(dem_filename) as fh:
+    dem = fh.read()
+    print(fh)
+    dem_bounds = fh.bounds
+    dem_crs = fh.crs
+contours = gpd.read_file(contours_filename)
+# Make sure that the coordinate reference system matches the image
+contours = contours.to_crs(dem_crs)
+         
 ##########################################################################
-###                      Load Greenland satellite image	               ###
-##########################################################################
-
-##########################################################################
-###                          Compute DEM contour	                       ###
-##########################################################################
-#Following the procedure from: https://www.earthdatascience.org/tutorials/visualize-digital-elevation-model-contours-matplotlib/
-
-#1. Read the data
-filename="C:/Users/jullienn/Documents/working_environment/greenland_topo_data/elevations/greenland_dem_mosaic_100m_v3.0.tif"
-gdal_data = gdal.Open(filename)
-gdal_band = gdal_data.GetRasterBand(1)
-nodataval = gdal_band.GetNoDataValue()
-
-#2. Some adjustments
-# convert to a numpy array
-data_array = gdal_data.ReadAsArray().astype(np.float)
-
-# replace missing values if necessary
-if np.any(data_array == nodataval):
-    data_array[data_array == nodataval] = np.nan
-
-#3. Plot quickly the data
-#Plot out data with Matplotlib's 'contour'
-fig = pyplot.figure(figsize = (12, 8))
-ax = fig.add_subplot(111)
-pyplot.contour(data_array, cmap = "viridis", 
-            levels = list(range(0, 3300, 1000)))
-pyplot.title("Elevation Contours of GrIS")
-cbar = pyplot.colorbar()
-pyplot.gca().set_aspect('equal', adjustable='box')
-pyplot.show()
-
-
-##########################################################################
-###                          Compute DEM contour	                       ###
+###                      Load Greenland DEM and contours               ###
 ##########################################################################
 
 
@@ -120,18 +82,16 @@ melt_year_plot=melt_year_plot[0,0,:,:,0]
 #melt_year_plot_binary=melt_year_plot[melt_year_plot<500]=0
 #melt_year_plot_binary=melt_year_plot[~(melt_year_plot<500)]=1
 
-#Plot excess melt
-pyplot.figure(figsize=(48,40))   
-#Change label font
-pyplot.rcParams.update({'font.size': 20})
-pyplot.grid()
-pyplot.imshow(np.flipud(melt_year_plot),extent=grid.extent,cmap='plasma_r')
-pyplot.clim(0,1000)
-pyplot.colorbar(label='Excess melt [mm w.e./year]')
-pyplot.title('Excess melt plot, year: '+wanted_year)
-pyplot.contour(np.flipud(data_array),extent=grid.extent,cmap = "viridis", 
-            levels = list(range(0, 3300, 1000)))
-                    
+#Plot dem and contours elevation
+plt.figure()
+ax = plt.subplot(111)
+dem_extent = (dem_bounds[0], dem_bounds[2], dem_bounds[1], dem_bounds[3])
+plt.imshow(np.squeeze(np.flipud(melt_year_plot)), extent=dem_extent,cmap='plasma_r')
+plt.colorbar(label='Excess melt [mm w.e./year]')
+plt.clim(0,1000)
+contours.plot(ax=ax, edgecolor='black')
+plt.title('Excess melt plot, year: '+wanted_year)
+
 ##Create the figure name
 #fig_name=[]
 #fig_name='C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/2002_2003_data_localisation/'+indiv_file+'.png'
@@ -139,6 +99,32 @@ pyplot.contour(np.flipud(data_array),extent=grid.extent,cmap = "viridis",
 ##Save the figure
 #pyplot.savefig(fig_name)
 #pyplot.clf()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ##########################################################################
 ###                          Plot excess melt data   	               ###
