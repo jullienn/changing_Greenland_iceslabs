ae3df9b4a6389d23603f94a2370c63a011211c3a
Nicolas Jullien <nicolas.jullien@unifr.ch>
2021-03-11 15:34:51 +0100

    Adding some improvements in ice lenses identification.

--- src/ice_lenses_identification_final.py
+++ src/ice_lenses_identification_final.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     build_coord_2002_3_file='FALSE'
     build_coord_2002_3_icelens_file='TRUE'
     
-    #file_to_investigate='may15_03_4_aggregated' #jun04_02proc_53.mat, may12_03_36, may11_03_20, jun04_02proc_52, jun04_02proc_4
+    #file_to_investigate='may18_02_27_aggregated' #jun04_02proc_53.mat, may12_03_36, may11_03_20, jun04_02proc_52, jun04_02proc_4
     
     if (identification == 'FALSE'):
         #Read the excel file:
