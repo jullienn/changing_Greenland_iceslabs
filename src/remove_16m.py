5dbd8907f14d3f58637693cce74ca036c81d6dbd
JULLIEN Nicolas <nicolas.jullien@unifr.ch>
2021-06-10 11:30:44 +0200

    Create the funtion remove_16m.py where I go through the'date_orig_CUTOFF_-0.45_THRESHOLD_000.png' files and identify the pixels where the ice content is higher than 16m after processing through 'IceBridgeGPRManager_v2.py'. Then I will take these identified exclusions and add them as exclusions into the the exclusion file 'LAKES_AND_OTHER_EXCLUSIONS.txt'. I will then rerun the processing with these new exclusions.

--- src/remove_16m.py
+++ src/remove_16m.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun  2 17:25:03 2021
+Created on Thu Jun 10 10:18:09 2021
 
 @author: JullienN
 """
@@ -33,7 +33,7 @@ def onclick(event):
 
 
 #Define the working environment
-path= 'C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/data/exported/2017_2018_09062021/'
+path= 'C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/data/exported/refine_exclusion/'
 
 f = open('C:/Users/jullienn/Documents/working_environment/iceslabs_MacFerrin/data/Exclusion_folder/data_2017_toberun.txt','r')
 dates_surf_2018 = [line.strip() for line in f.readlines() if len(line.strip()) > 0]
@@ -41,19 +41,46 @@ f.close()
 
 for indiv_file in list(dates_surf_2018):
     print(indiv_file)
-    #Open only the roll_corrected files of interest
-    filename=indiv_file+'_ROLLCORRECT__BEFORE.png'
+    #Open only the _orig_CUTOFF_-0.45_THRESHOLD_000 files of interest
+    filename=indiv_file+'_orig_CUTOFF_-0.45_THRESHOLD_000.png'
     
-    #Open and plot image
-    ROLLCORRECT__BEFORE = Image.open(path+filename).convert("L")
-    arr_ROLLCORRECT__BEFORE = np.asarray(ROLLCORRECT__BEFORE)
-    #Prepare plot$
+    #Open image
+    file_to_show = Image.open(path+filename).convert("L")
+    arr_file_to_show = np.asarray(file_to_show)
     
+    #Convert arr_file_to_show into a boolean (from 0 and 255 to 1 and 0).
+    #When pixel=255, it means white. When pixel=0, it means black. I want to
+    #count the number of times I have blacks pixels (ice), thus I need to
+    #transform 255 into 0 and 0 in 1. This is what is done below.
+    
+    arr_file_to_show_boolean=arr_file_to_show==0
+    arr_file_to_show_boolean=arr_file_to_show_boolean*1
+    
+    #Retreive the 16m depth index
+    depth20m=arr_file_to_show_boolean.shape[0]
+    depth16m=arr_file_to_show_boolean.shape[0]*16/20
+    
+    exclusion=[]
+    exclusion_vect=np.zeros(arr_file_to_show_boolean.shape[1])
+    exclusion_vect[:]=np.nan
+        
+    #Check if ice content >16m on each vertial column. If yes, create an exclusion pixel
+    for i in range(0,arr_file_to_show_boolean.shape[1]):
+        if (np.sum(arr_file_to_show_boolean[:,i])>=depth16m):
+            #more than 16m of ice in this vertical column, retreive exclusion pixel
+            exclusion=np.append(exclusion,i)
+            exclusion_vect[i]=i
+    
+    #Create a visualisation exclusion vector
+    #Prepare plot
     fig, (ax1) = plt.subplots(1, 1)#, gridspec_kw={'width_ratios': [1, 3]})
     fig.suptitle(indiv_file)
-    ax1.imshow(arr_ROLLCORRECT__BEFORE, cmap='gray', vmin=0, vmax=255)
+    ax1.imshow(arr_file_to_show, cmap='gray', vmin=0, vmax=255)
     fig.canvas.mpl_connect('button_press_event', onclick)
-    plt.show()
     
+    #Show the exclusion
+    ax1.plot(exclusion_vect,np.ones(arr_file_to_show_boolean.shape[1]))   
+    
+    plt.show()
     pdb.set_trace()
-
+    
