939e688171675f55355c1bb4d94df966d2606f56
JULLIEN Nicolas <nicolas.jullien@unifr.ch>
2021-09-16 12:50:26 +0200

    Created the code 'iceslabs_spatial_aggregation_grid.py' where I do the spatial aggregation of 2010-2018 data over a grid.

---
+++ src/iceslabs_spatial_aggregation_grid.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 09:20:00 2021
+
+@author: JullienN
+"""
+
+##############################################################################
+############### Define function for discrete colorbar display ###############
+##############################################################################
+def discrete_cmap(N, base_cmap=None):
+    """Create an N-bin discrete colormap from the specified input map"""
+    #This piece of code is from: https://gist.github.com/jakevdp/91077b0cae40f8f8244a
+    # Note that if base_cmap is a string or None, you can simply do
+    #    return plt.cm.get_cmap(base_cmap, N)
+    # The following works for string, None, or a colormap instance:
+    
+    base = plt.cm.get_cmap(base_cmap)
+    color_list = base(np.linspace(0, 1, N))
+    cmap_name = base.name + str(N)
+    return base.from_list(cmap_name, color_list, N)
+##############################################################################
+############### Define function for discrete colorbar display ###############
+##############################################################################
+
+#Import librairies
+import pandas as pd
+import datetime
+from scipy import spatial
+import pdb
+import numpy as np
+from pyproj import Transformer
+import matplotlib.pyplot as plt
+from pysheds.grid import Grid
+import matplotlib.colors as mcolors
+
+#Load all 2010-2018 data
+path='C:/Users/jullienn/switchdrive/Private/research/RT1/final_dataset_2010_2018/excel_files/'
+df_2010_2018 = pd.read_csv(path+'jullienetal_20102018.csv',delimiter=';',decimal=',')
+
+#Create a year column
+year=df_2010_2018.Track_name.str[:4]
+df_2010_2018['year']=year.astype(str).astype(int)
+
+#Transform the coordinated from WGS84 to EPSG:3413
+transformer = Transformer.from_crs("EPSG:4326", "EPSG:3413", always_xy=True)
+points=transformer.transform(np.asarray(df_2010_2018["lon"]),np.asarray(df_2010_2018["lat"]))
+
+#Store lat/lon in 3413
+df_2010_2018['lon_3413']=points[0]
+df_2010_2018['lat_3413']=points[1]
+
+#Visualize the spatial aggregation process
+########################## Load GrIS elevation ##########################
+#Open the DEM
+path_raster='C:/Users/jullienn/switchdrive/Private/research/backup_Aglaja/working_environment/greenland_topo_data/elevations/'
+grid = Grid.from_raster(path_raster+"greenland_dem_mosaic_100m_v3.0.tif",data_name='dem')
+#Minnor slicing on borders to enhance colorbars
+elevDem=grid.dem[:-1,:-1]              
+#Scale the colormap
+divnorm = mcolors.DivergingNorm(vmin=0, vcenter=1250, vmax=2500)
+########################## Load GrIS elevation ##########################
+
+fig, (ax1) = plt.subplots(1, 1)#, gridspec_kw={'width_ratios': [1, 3]})
+fig.suptitle('Spatial aggregation illustration')
+#Display DEM
+cb1=ax1.imshow(elevDem, extent=grid.extent,cmap=discrete_cmap(10,'cubehelix_r'),alpha=0.5,norm=divnorm)
+cbar1=fig.colorbar(cb1, ax=[ax1], location='left')
+cbar1.set_label('Elevation [m]')
+#Display 2010-2018 data
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2010']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2010']['lat_3413'],s=0.1,color='#525252',label='2010-2014')
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2011']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2011']['lat_3413'],s=0.1,color='#525252')
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2012']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2012']['lat_3413'],s=0.1,color='#525252')
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2013']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2013']['lat_3413'],s=0.1,color='#525252')
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2014']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2014']['lat_3413'],s=0.1,color='#525252')
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2017']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2017']['lat_3413'],s=0.1,color='#d9d9d9',label='2017-2018')
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2018']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2018']['lat_3413'],s=0.1,color='#d9d9d9')
+#Display the aggregation data
+plt.legend()
+plt.show()
+
+
+#Create the grid
+a=1000 #length of one side of the cell
+b=1000 #lenght of the other side of the cell
+
+lat_vector=np.arange(np.min(df_2010_2018['lat_3413']),np.max(df_2010_2018['lat_3413']),a)
+lon_vector=np.arange(np.min(df_2010_2018['lon_3413']),np.max(df_2010_2018['lon_3413']),b)
+
+grid_for_aggregation=np.meshgrid(lon_vector,lat_vector)
+points=np.c_[grid_for_aggregation[0].ravel(), grid_for_aggregation[1].ravel()]
+
+#Radius to search is: r = sqrt(a^2+b^2)/2. We round up to the next integer to be conservative
+radius_search=np.ceil(np.sqrt(a*a+b*b)/2)
+######################################################################
+###         Begin from spatial_aggregation_2010_2018.py            ###
+#Define coordinates tuples
+XY_iceslabs = np.array((df_2010_2018['lon_3413'],df_2010_2018['lat_3413'])).swapaxes(0,1)
+
+#Define the look up tree
+tree = spatial.cKDTree(XY_iceslabs)
+
+#This is from : https://medium.com/nam-r/10-essential-operations-for-spatial-data-in-python-4603d933bdda, point 7
+#Find all the point that lies within the circle of radius r centered around the point of reference
+neigh_list=[]
+for count, value in enumerate(points):
+    neigh_list.append(tree.query_ball_point(value,r=radius_search))
+    print(count/len(points)*100,'%')
+
+#Create the vectors to store the aggregated data
+avg_20m_icecontent=np.zeros(len(neigh_list))
+avg_20m_icecontent[:]=np.nan
+
+avg_lat_3413=np.zeros(len(neigh_list))
+avg_lat_3413[:]=np.nan
+
+avg_lon_3413=np.zeros(len(neigh_list))
+avg_lon_3413[:]=np.nan
+
+year_data=np.zeros(len(neigh_list))
+year_data[:]=np.nan
+
+keys=np.zeros(len(neigh_list))
+keys[:]=np.nan
+
+#Set key to zero
+key = 0
+
+#initialize count to 0
+count=0
+
+#Initialise ite for lat/lon of grid retrieval
+ite=0
+
+for index_list in neigh_list:
+
+    #retreive all data which index belong to index_list
+    df_temp=df_2010_2018.iloc[index_list]
+    
+    if (len(df_temp) == 0):
+        #no data at this grid point, continue
+        #Update ite
+        ite=ite+1
+        continue
+    else:
+        #data in this grid point, average data
+        #Loop over the years for aggregation
+        for year in list(np.unique(df_temp['year'])):
+            #average the 20m ice content for each year
+            ice_avg=np.average(df_temp[df_temp['year']==year]['20m_ice_content_m'])
+            #take the lat/lon of the gris point
+            lat_avg=points[ite][1]
+            lon_avg=points[ite][0]
+            
+            #Store the results
+            avg_20m_icecontent[count]=ice_avg
+            avg_lat_3413[count]=lat_avg
+            avg_lon_3413[count]=lon_avg
+            year_data[count]=year
+            keys[count]=key
+            
+            #Update count
+            count=count+1
+        
+        #Update ite
+        ite=ite+1
+        
+        #Update key
+        key=key+1
+        
+    print(ite/len(neigh_list)*100,' %')
+    
+#Remove nans
+avg_20m_icecontent=avg_20m_icecontent[~np.isnan(avg_20m_icecontent)]
+avg_lat_3413=avg_lat_3413[~np.isnan(avg_lat_3413)]
+avg_lon_3413=avg_lon_3413[~np.isnan(avg_lon_3413)]
+year_data=year_data[~np.isnan(year_data)]
+keys=keys[~np.isnan(keys)]
+
+#Create a pd dataframe
+df_spatial_aggregation=pd.DataFrame({'avg_20m_icecontent':avg_20m_icecontent,
+                                     'avg_lat_3413':avg_lat_3413,
+                                     'avg_lon_3413':avg_lon_3413,
+                                     'year':year_data,
+                                     'key':keys})
+
+'''
+#Save df as excel file
+filename_to_save='jullien_etal_20102018_spatial_aggregation'+str(custom_radius)+'.xlsx'
+df_spatial_aggregation.to_excel('C:/Users/jullienn/switchdrive/Private/research/RT1/final_dataset_2010_2018/excel_files/'+filename_to_save)
+'''
+    
+
+###         End from spatial_aggregation_2010_2018.py            ###
+######################################################################
+
+
+
--- src/lenses_thickening_visualisation.py.py
+++ src/lenses_thickening_visualisation.py.py
@@ -60,6 +60,7 @@ from matplotlib import pyplot
 from PIL import Image
 from pyproj import Transformer
 import xarray as xr
+from pysheds.grid import Grid
 
 ##############################################################################
 ###                     Define what we want to show                        ###
@@ -740,6 +741,165 @@ for single_year in investigation_year.keys():
 '''
 
 pdb.set_trace()
+
+##############################################################################
+###   This part of code is from iceslabs_20102018_thickening_analysis.py   ###
+##############################################################################
+#Import librairies
+from scipy import spatial
+import pandas as pd
+import matplotlib.colors as mcolors
+
+custom_radius=1000
+
+#Load the spatial aggregated data. All the points within a radius of 100m are averaged
+path='C:/Users/jullienn/switchdrive/Private/research/RT1/final_dataset_2010_2018/excel_files/'
+df_2010_2018_spatially_aggregated = pd.read_csv(path+'jullien_etal_20102018_spatial_aggregation'+str(custom_radius)+'.csv',delimiter=';',decimal=',')
+
+#Load all 2010-2018 data without spatial aggregation
+path='C:/Users/jullienn/switchdrive/Private/research/RT1/final_dataset_2010_2018/excel_files/'
+df_2010_2018 = pd.read_csv(path+'jullienetal_20102018.csv',delimiter=';',decimal=',')
+#Transform the coordinated from WGS84 to EPSG:3413
+transformer = Transformer.from_crs("EPSG:4326", "EPSG:3413", always_xy=True)
+points=transformer.transform(np.asarray(df_2010_2018["lon"]),np.asarray(df_2010_2018["lat"]))
+
+#Store lat/lon in 3413
+df_2010_2018['lon_3413']=points[0]
+df_2010_2018['lat_3413']=points[1]
+
+#Visualize the spatial aggregation process
+########################## Load GrIS elevation ##########################
+#Open the DEM
+grid = Grid.from_raster("C:/Users/jullienn/switchdrive/Private/research/backup_Aglaja/working_environment/greenland_topo_data/elevations/greenland_dem_mosaic_100m_v3.0.tif",data_name='dem')
+#Minnor slicing on borders to enhance colorbars
+elevDem=grid.dem[:-1,:-1]              
+#Scale the colormap
+divnorm = mcolors.DivergingNorm(vmin=0, vcenter=1250, vmax=2500)
+########################## Load GrIS elevation ##########################
+
+import datetime as dt
+import statsmodels.api as sm
+import datetime
+from scipy import stats
+from mpl_toolkits.mplot3d import Axes3D
+
+# --------------- Calculate the slope of each aggregated point --------------- #
+# Create empty vectors
+slope_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+slope_vect[:]=np.nan
+
+rvalue_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+rvalue_vect[:]=np.nan
+
+pvalue_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+pvalue_vect[:]=np.nan
+
+stderr_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+stderr_vect[:]=np.nan
+
+lat_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+lat_vect[:]=np.nan
+
+lon_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+lon_vect[:]=np.nan
+
+key_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+key_vect[:]=np.nan
+
+nb_vect=np.zeros(len(df_2010_2018_spatially_aggregated))
+nb_vect[:]=np.nan
+
+#Calculate the slope:
+#count for datastorage
+count=0
+#Loop over the keys
+for k in range(0,len(pd.unique(df_2010_2018_spatially_aggregated['key']))):
+    #Define the key bounds
+    key_bound=str('key=='+str(k))
+    
+    #Filter the data accoring to key bin
+    df_temp=df_2010_2018_spatially_aggregated.query(key_bound)
+    
+    #Check if there are data:
+    check=df_temp['year'].to_numpy()
+    if (len(np.unique(check))>1): #there are more than one year of data, growth
+    #rate calcultation possible!
+        # -------------- Calculate slope using all the data -------------- #
+        #Convert x and y to array for the slope calculation
+        ice_thickness_array_temp=df_temp['avg_20m_icecontent'].to_numpy()
+        year_array_temp=df_temp['year'].to_numpy()
+
+        #Calculate the growth rate - any data point is considered in the
+        #growth rate calculation
+        slope, intercept, r_value, p_value, std_err = stats.linregress(year_array_temp,ice_thickness_array_temp)
+
+        ##plot the data and fitted slope
+        # plt.plot(year_array_temp,ice_thickness_array_temp,'o',label='original data')
+        # plt.plot(year_array_temp,intercept+slope*year_array_temp,'r',label='fitted line')
+        # plt.legend()
+        # plt.show()
+        # -------------- Calculate slope using all the data -------------- #
+
+        #Store the results from the linear regression fonction into df_slope
+        slope_vect[count]=slope
+        rvalue_vect[count]=r_value
+        pvalue_vect[count]=p_value
+        stderr_vect[count]=std_err
+        nb_vect[count]=len(np.unique(check))
+
+        lat_vect[count]=df_temp.avg_lat_3413.iloc[0]
+        lon_vect[count]=df_temp.avg_lon_3413.iloc[0]
+        key_vect[count]=df_temp.key.iloc[0]
+
+        count=count+1
+    
+    print(k/len(pd.unique(df_2010_2018_spatially_aggregated['key']))*100,' %')
+
+#Create a dataframe where the slope calculation are stored
+column_names = ["lat_3413","lon_3413","slope","key","nb_year","r_value","p_value","std_err"]
+df_slope = pd.DataFrame(columns = column_names)
+#Store the resulting slope, p_value, r_value, std_err into a dataframe
+df_slope.lat_3413=lat_vect
+df_slope.lon_3413=lon_vect
+df_slope.key=key_vect
+df_slope.nb_year=nb_vect
+df_slope.slope=slope_vect
+df_slope.r_value=rvalue_vect
+df_slope.p_value=pvalue_vect
+df_slope.std_err=stderr_vect
+
+#Display where overlapping = 7 years.
+df_slope_7years=df_slope[df_slope.nb_year==7]
+
+# Make the norm for slope plotting
+divnorm_slope = mcolors.DivergingNorm(vmin=-2, vcenter=0, vmax=2)
+# ------ Display slope for overlaping years >2 
+fig, (ax1) = plt.subplots(1, 1)#, gridspec_kw={'width_ratios': [1, 3]})
+fig.suptitle('Spatial aggregation slope, overlapping = 7 years')
+#Display DEM
+cb1=ax1.imshow(elevDem, extent=grid.extent,cmap=discrete_cmap(10,'cubehelix_r'),alpha=0.5,norm=divnorm)
+cbar1=fig.colorbar(cb1, ax=[ax1], location='left')
+cbar1.set_label('Elevation [m]')
+#Display 2017-2018 data
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2017']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2017']['lat_3413'],s=0.1,color='#d9d9d9',label='2017-2018')
+plt.scatter(df_2010_2018[df_2010_2018.Track_name.str[:4]=='2018']['lon_3413'],df_2010_2018[df_2010_2018.Track_name.str[:4]=='2018']['lat_3413'],s=0.1,color='#d9d9d9')
+#Display the 7 years aggregation data
+sc= ax1.scatter(df_slope_7years['lon_3413'],df_slope_7years['lat_3413'],c=df_slope_7years['slope'],cmap='seismic_r',norm=divnorm_slope,label='2010-2018 slope, overlapping = 7years')
+cbar=fig.colorbar(sc)
+cbar.set_label('Growth rate [m/year]', fontsize=15)
+plt.legend()
+ax1.set_xlim(-115000,-80000)
+ax1.set_ylim(-2470000,-2440000)
+plt.show()
+
+
+pdb.set_trace()
+
+
+##############################################################################
+###   This part of code is from iceslabs_20102018_thickening_analysis.py   ###
+##############################################################################
+
 '''
 ##########################################################################
 ###                  Calculate cumulative ice slabs                    ###
